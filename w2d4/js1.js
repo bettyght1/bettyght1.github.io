9e625d2631433ddf1bd026f90a69620236ab5d26
bettyght1 <bdesta@miu.edu>
2021-07-30 09:45:47 -0500

    w2d4 final

--- w2d4/js1.js
+++ w2d4/js1.js
@@ -47,8 +47,9 @@ var teacher = new Teacher();
 teacher.initialize("Joe", 45);
 
 
-console.log("Q1:- chai has a lot of stuff, let's make assert global".filter("has ", 'let\'s ', 'assert'));
+
+console.log("Q1:- This is not and never don't nice house".filter("not ", 'never ', 'don\'t '));
 
 console.log("Q2:- Sorted array = "+[6,4,0, 3,-2,1].bubbleSort()); //[-2, 0, 1, 3, 4, 6]
 
-console.log("Q3:- "+teacher.teach("WAP"));
+console.log("Q3:- "+teacher.teach("Inheritance"));
